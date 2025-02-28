14cedd564f433fe6993a66a1d06ef74c6084d0a9
sufyan <csc21s135@gmail.com>
2025-03-01 01:40:26 +0500

    last update

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App.jsx";
 import "./index.css";
 import Layout from "./Layout.jsx";
 import Home from "./pages/Home.jsx";
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from './Components/Navbar'
+import { Outlet } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 const Layout = () => {
-    return (
-        <>
-            <Navbar />
-            <Outlet />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
