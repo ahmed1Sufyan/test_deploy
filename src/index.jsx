412aaf5ac74ff5cae464101f4bde3dc7212453d2
sufyan <csc21s135@gmail.com>
2025-03-01 01:25:10 +0500

    improved

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
-import Layout from "./layout.jsx";
+import Layout from "./Layout.jsx";
 import Home from "./pages/Home.jsx";
 // import Login from "./pages/Login.jsx";
 // import Register from './pages/Register.jsx'
